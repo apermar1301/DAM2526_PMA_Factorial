af26441587e113c104896d90d37b9400efcb6f65
apermar1301 <apermar1301@g.educaand.es>
2025-12-10 09:11:28 +0100

    Versión D: Mejora de la función pedirNumero() y uso de Scanner.hasNextInt()

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -19,9 +19,24 @@ public class Factorial {
         System.out.println("RESULTADO - En versiones posteriores se calculará el factorial del número " + num);
 	}
 	
-	public static int pedirNumero() {
-		System.out.print("Introduce un número entero mayor o igual que 0: ");
-        return sc.nextInt();
+	private static int pedirNumero() {
+	    int num;
+	    boolean valido;
+
+	    do {
+	        System.out.print("Introduce un número entero mayor o igual que 0: ");
+	        if (sc.hasNextInt()) {
+	            num = sc.nextInt();
+	            valido = true;
+	        } else {
+	            System.out.println("Entrada no válida. Introduce un entero.");
+	            sc.next();
+	            valido = false;
+	            num = -1;
+	        }
+	    } while (!valido);
+
+	    return num;
 	}
 
 }
